fff6622bcfc57bc432c3fd039589346f0f1d00ab
dorotas <d.suchocka@gmail.com>
2012-10-27 06:59:42 +0200

    testZ2

--- test.rb
+++ test.rb
@@ -22,8 +22,10 @@ def set_up
   sun.originate(p2)
   p3 = Packet.new("pack3", "Sun", "PC")
   sun.originate(p3)
-  p4 = Packet.new("pack4", "Mac", "Bother")
-  mac.originate(p4)
+  p5 = Packet.new("pack4", "Mac", "Bother")
+  mac.originate(p5)
+  p6 = Packet.new("pack5", "PC", "SomeF")
+  pc.originate(p6)
 
   p4 = Packet.new("testpack", "Mac", "Sun")
   print "Pakiet #{p4.contents} zostal wyslany przez stacje: "
@@ -58,16 +60,61 @@ if !s1.has_next_node
 end
 s2 = Workstation.new("S2")
 puts "Poprawna stacja powstala"
+s2.next_node = s1
 if s2.has_next_node
   puts "Stacja ma nastepnik"
 end
 begin
   Workstation.new("")
 rescue
-  puts "Niepoprawna stacja nie powstala"
+  puts "Niepoprawna stacja nie powstała"
 end
 begin
   Workstation.new("S3")
+rescue
+  puts "Niepoprawna stacja nie powstała"
+end
+
+p1 = Printer.new("P1")
+puts "Poprawna drukarka powstala"
+if !p1.has_next_node
+  puts "Drukarka nie ma nastepnika"
+end
+p2 = Printer.new("P2")
+puts "Poprawna drukarka powstala"
+p2.next_node = s2
+if p2.has_next_node
+  puts "Drukarka ma nastepnik"
+end
+begin
+  Printer.new("")
+rescue
+  puts "Niepoprawna drukarka nie powstala"
+end
+begin
+  Printer.new("S3")
+rescue
+  puts "Niepoprawna drukarka nie powstala"
+end
+
+f1 = Fileserver.new("F1")
+puts "Poprawny system plików powstał"
+if !f1.has_next_node
+  puts "System plików nie ma nastepnika"
+end
+f2 = Workstation.new("F2")
+puts "Poprawny system plików powstał"
+f2.next_node = p2
+if f2.has_next_node
+  puts "System plików ma nastepnik"
+end
+begin
+  Fileserver.new("")
+rescue
+  puts "Niepoprawna stacja nie powstala"
+end
+begin
+  Fileserver.new("F3")
 rescue
   puts "Niepoprawna stacja nie powstala"
 end
